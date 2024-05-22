17f91577b7670f37de2c6803d564c8abdef82720
sachinkuravanweb21 <sachinkuravanweb21@gmail.com>
2024-05-22 12:14:19 +0530

    modified files

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function toggleMobileMenu(){
-    document.getElementById("menu").classList.toggle("active");
+function toggleMobileMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
+
+// map
+
+function initMap() {
+  const mapOptions = {
+    center: { lat: 37.7749, lng: -122.4194 }, // Coordinates of San Francisco
+    zoom: 12,
+  };
+
+  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  // Adding a marker
+  const marker = new google.maps.Marker({
+    position: { lat: 23.44, lng: -122.4194 },
+    map: map,
+    title: "San Francisco",
+  });
 }
